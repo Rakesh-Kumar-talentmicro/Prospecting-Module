a005a3210860e3804255b05ddeedcb5b4e19bde8
sheikhsatru <shahbazahmad1439@gmail.com>
2026-05-08 02:24:13 +0530

    Add MySQL connection status logs

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',   
+  host: process.env.DB_HOST || '127.0.0.1',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
@@ -12,5 +13,17 @@ const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// Check Database Connection
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    console.log(" MySQL Database Connected Successfully");
+    connection.release();
+  } catch (error) {
+    console.error(" Database Connection Failed");
+    console.error(error.message);
+  }
+})();
 
 export default db;
